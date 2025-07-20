c77a7165a4c4af47e7b62bc79c2c9774c1691567
saucoide <saucoide@gmail.com>
2025-07-20 15:52:45 +0200

    silence httpx

--- nox/logger.py
+++ nox/logger.py
@@ -142,3 +142,5 @@ def setup_logging(
 
     # Silence noisy loggers
     logging.getLogger("sh").setLevel(logging.WARNING)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("httpxcore").setLevel(logging.WARNING)
