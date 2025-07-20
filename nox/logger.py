0947332f598ef0da6e0fabe464207205bcd84348
saucoide <saucoide@gmail.com>
2025-07-20 17:21:48 +0200

    even less noise

--- nox/logger.py
+++ nox/logger.py
@@ -150,3 +150,4 @@ def setup_logging(
     logging.getLogger("sh").setLevel(logging.WARNING)
     logging.getLogger("httpx").setLevel(logging.WARNING)
     logging.getLogger("httpcore").setLevel(logging.WARNING)
+    logging.getLogger("pbs_installer").setLevel(logging.WARNING)
